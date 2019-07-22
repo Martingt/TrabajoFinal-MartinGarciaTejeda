71009f6ee9027c6ee4d80e55ecd86c5b15511a47
Martin Garcia Tejeda <m.garcia.tejeda@gmail.com>
2019-07-21 23:13:18 -0300

    Se agrego tests para mostrar que se puede crear una carpeta dentro de otra

--- src/main.js
+++ src/main.js
@@ -1,21 +1,44 @@
 var prompt = require('prompt');
+const {TerminalWindow, Directory, User} = require('../src/model');
  
-// class TerminalWindow {
+// let terminal;
+// let martin = new User('Martin');
+// let rootUser = new User('root');
+// let root = new Directory('/', rootUser);
 
-// }
-//
-// Start the prompt
-//
-prompt.start();
+// terminal = new TerminalWindow(martin, root);
+// terminal.createFolder('Movies');
 
-//
-// Get two properties from the user: username and email
-//
-prompt.get(['pt', 'email'], function (err, result) {
-  //
-  // Log the results.
-  //
-  console.log('Command-line input received:');
-  console.log('  username: ' + result.username);
-  console.log('  email: ' + result.email);
-});
+// let directories = terminal.getWorkingDirectory().getSubdirectories();
+//         //let files = terminal.getWorkingDirectory().getFiles();
+//         let output = '';
+//         for(let directory of directories) {
+//             output = output + directory.getName() + '\n';
+//         }
+
+// console.log(terminal.getWorkingDirectory());
+// console.log(output);
+// console.log('Movies'+'\n');
+// console.log(terminal.ls());
+
+
+// // class TerminalWindow {
+
+// // }
+// //
+// // Start the prompt
+// //
+// console.log('puto');
+// prompt.start();
+
+// //
+// // Get two properties from the user: username and email
+// //
+// prompt.get(['pt', 'email'], function (err, result) {
+//   //
+//   // Log the results.
+//   //
+//   console.log('Command-line input received:');
+//   console.log('  username: ' + result.username);
+//   console.log('  email: ' + result.email);
+// });
