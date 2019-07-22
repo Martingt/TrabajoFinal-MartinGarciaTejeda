24cc13b08fd4ea22ed883da8885bee7a707ad931
Martin Garcia Tejeda <m.garcia.tejeda@gmail.com>
2019-07-22 16:45:36 -0300

    Se creo una base de datos con usuarios y sus contraseñas. Tests de la base de datos y mas tests en terminal

--- src/main.js
+++ src/main.js
@@ -1,44 +1,90 @@
 var prompt = require('prompt');
-const {TerminalWindow, Directory, User} = require('../src/model');
- 
-// let terminal;
-// let martin = new User('Martin');
-// let rootUser = new User('root');
-// let root = new Directory('/', rootUser);
+const {TerminalWindow, Directory, DataBase, User, isUserNameValid} = require('../src/model');
+const uuidV1 = require('uuid/v1');
 
-// terminal = new TerminalWindow(martin, root);
-// terminal.createFolder('Movies');
+let dataBase
+    let terminal;
+    
+    dataBase = new DataBase();
+    dataBase.createUser('martin', 'Martinlgt1');
+    dataBase.createUser('pedro123', 'pedro1');
+    dataBase.createUser('juan1234', 'juan1');
+    dataBase.createUser('lucas1234', 'lucas');
+    let rootUser = dataBase.getUsers().get('root')
+    let martin = dataBase.getUsers().get('martin');
+    let pedro = dataBase.getUsers().get('pedro123');
+    let juan = dataBase.getUsers().get('juan1234');
+    let lucas = dataBase.getUsers().get('lucas1234');
+    let root = new Directory('/', rootUser);
+    let autos = new Directory('autos', pedro);
+    let series = new Directory('series', juan);
+    let peugeot = new Directory('peugeot', lucas);
+    root.addDirectory(autos);
+    root.addDirectory(series);
+    autos.addDirectory(peugeot);
 
-// let directories = terminal.getWorkingDirectory().getSubdirectories();
-//         //let files = terminal.getWorkingDirectory().getFiles();
-//         let output = '';
-//         for(let directory of directories) {
-//             output = output + directory.getName() + '\n';
-//         }
+    terminal = new TerminalWindow(rootUser, root, dataBase); 
 
-// console.log(terminal.getWorkingDirectory());
-// console.log(output);
-// console.log('Movies'+'\n');
-// console.log(terminal.ls());
+    terminal.loginAs('martin', 'Martinlgt1');
 
+    console.log(terminal.getUser());
+    console.log(martin);
+        
+//   readline.question(`What's your name?`, (name) => {
+//     console.log(`Hi ${name}!`)
+//     readline.close()
+//   })
 
-// // class TerminalWindow {
+// console.log(uuidV1());
+
+// let map = new Map();
+// //let caca = [1 : 'pepe'};
+// map.set(1,'pepe');
+// map.set('hi','choto');
+
+// // while('./exit') {
 
 // // }
-// //
-// // Start the prompt
-// //
-// console.log('puto');
-// prompt.start();
 
-// //
-// // Get two properties from the user: username and email
-// //
-// prompt.get(['pt', 'email'], function (err, result) {
-//   //
-//   // Log the results.
-//   //
-//   console.log('Command-line input received:');
-//   console.log('  username: ' + result.username);
-//   console.log('  email: ' + result.email);
-// });
+// console.log(map);
+// // let terminal;
+// // let martin = new User('Martin');
+// // let rootUser = new User('root');
+// // let root = new Directory('/', rootUser);
+
+// // terminal = new TerminalWindow(martin, root);
+// // terminal.createFolder('Movies');
+
+// // let directories = terminal.getWorkingDirectory().getSubdirectories();
+// //         //let files = terminal.getWorkingDirectory().getFiles();
+// //         let output = '';
+// //         for(let directory of directories) {
+// //             output = output + directory.getName() + '\n';
+// //         }
+
+// // console.log(terminal.getWorkingDirectory());
+// // console.log(output);
+// // console.log('Movies'+'\n');
+// // console.log(terminal.ls());
+
+
+// // // class TerminalWindow {
+
+// // // }
+// // //
+// // // Start the prompt
+// // //
+// // console.log('puto');
+// // prompt.start();
+
+// // //
+// // // Get two properties from the user: username and email
+// // //
+// // prompt.get(['pt', 'email'], function (err, result) {
+// //   //
+// //   // Log the results.
+// //   //
+// //   console.log('Command-line input received:');
+// //   console.log('  username: ' + result.username);
+// //   console.log('  email: ' + result.email);
+// // });
--- tests/terminalwindow.spec.js
+++ tests/terminalwindow.spec.js
@@ -1,37 +1,95 @@
-const {TerminalWindow, Directory, User} = require('../src/model');
+const {TerminalWindow, Directory, User, DataBase} = require('../src/model');
 const {expect, assert} = require('chai');
+
 // var expect = require('chai').expect;
 // var assert = require('chai').assert;
 // var should = require('chai').should();
 
-// describe('Data base', () => {
-//     let db;
-//     beforeEach(() => {
-//         db = new dataBase();
-//     });
-// });
+describe('Data base', () => {
+    let db;
+    beforeEach(() => {
+        db = new DataBase();
+    });
+    it('Should have a root user', () => {
+        expect(db.getUsers().has('root')).is.true;
+    });
+    it('Should create a user named martin', () => {
+        db.createUser('martin', '12345');
+        expect(db.getUsers().has('martin')).is.true;
+    });
+    it('Should create a user named coca_2', () => {
+        db.createUser('coca_2', 'pepsi');
+        expect(db.getUsers().has('coca_2')).is.true;
+    });
+    it('Should not create a user named coca_2!', () => {
+        db.createUser('coca_2!', 'pepsi');
+        expect(db.getUsers().has('coca_2!')).is.false;
+    });
+    it('Should not create a user named Coca_2', () => {
+        db.createUser('Coca_2', 'pepsi');
+        expect(db.getUsers().has('Coca_2')).is.false;
+    });
+    it('Should not create a user named coca becouse the length name is 4', () => {
+        db.createUser('coca', 'pepsi');
+        expect(db.getUsers().has('coca')).is.false;
+    });
+    it('Should not create a user named martinlaureanogarciatejeda becouse the length name is 26', () => {
+        db.createUser('martinlaureanogarciatejeda', 'pepsi');
+        expect(db.getUsers().has('martinlaureanogarciatejeda')).is.false;
+    });
+    it('Should create a user named martinlaureanogarciatejed becouse the length name is 25', () => {
+        db.createUser('martinlaureanogarciatejed', 'pepsi');
+        expect(db.getUsers().has('martinlaureanogarciatejed')).is.true;
+    });
+    it('Should create a user named 123456 becouse the length name is 6', () => {
+        db.createUser('123456', 'pepsi');
+        expect(db.getUsers().has('123456')).is.true;
+    });
+    it('Should not create duplicated user', () => {
+        db.createUser('martin', 'pepsi');
+        db.createUser('martin', 'coca');
+        db.createUser('martin', 'manaos');
+        expect(db.getUsers().size).equal(2);
+    });
+});
 describe('Terminal window', () => {
     let terminal;
-    let martin = new User('Martin');
-    let rootUser = new User('root');
+    let dataBase = new DataBase();
+    dataBase.createUser('martin', 'Martinlgt1');
+    dataBase.createUser('pedro123', 'pedro1');
+    dataBase.createUser('juan1234', 'juan1');
+    dataBase.createUser('lucas1234', 'lucas');
+    let rootUser = dataBase.getUsers().get('root')
+    let martin = dataBase.getUsers().get('martin');
+    let pedro = dataBase.getUsers().get('pedro123');
+    let juan = dataBase.getUsers().get('juan1234');
+    let lucas = dataBase.getUsers().get('lucas1234');
     let root = new Directory('/', rootUser);
+    let autos = new Directory('autos', pedro);
+    let series = new Directory('series', juan);
+    let peugeot = new Directory('peugeot', lucas);
+    root.addDirectory(autos);
+    root.addDirectory(series);
+    autos.addDirectory(peugeot);
+
     beforeEach(() => {  
-        terminal = new TerminalWindow(martin, root); 
+        terminal = new TerminalWindow(rootUser, root, dataBase); 
     });
     it('Should show Martin as user login', () => {
-        terminal.loginAs('Martin', '12345');
+        terminal.loginAs('martin', 'Martinlgt1');
         expect(terminal.getUser()).equal(martin);
-        expect(terminal.getUserName()).equal('Martin');
+        expect(terminal.getUserName()).equal('martin');
     });
     it('Should show the user on the root directory', () => {
         expect(terminal.printWorkingDirectory()).equal('/');
     });
     it('Should create a folder named Movies owned by the user', () => {
-        let moviesFolder = new Directory('Movies');
-        let lsOutputExpected = [moviesFolder];
         terminal.createFolder('Movies');
-        expect(terminal.ls()).equal('Movies'+'\n');
+        expect(terminal.ls()).equal('autos\nseries\nMovies'+'\n');
     });
+    it('Should create a user by the root', () => {
+        expect(terminal.getUser().createUser('messi_barsa', 'barcelona', terminal.getDB())).equal('El usuario messi_barsa ha sido creado');
+    })
 });
 describe('Directory description', () => {
     let directory;
